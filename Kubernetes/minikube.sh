589ca788e1d97c45f85d5fe3217618bcc3db91f0
DaMuffinDev <davidwagner224@gmail.com>
2022-11-16 13:14:40 -0600

    Docker and Minikube fix

--- Kubernetes/minikube.sh
+++ Kubernetes/minikube.sh
@@ -1,5 +1,9 @@
 sudo apt update && sudo apt upgrade
+
+# Install Dependencies
 sudo apt install conntrack
+source ../docker.sh
+
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
 sudo sysctl net/netfilter/nf_conntrack_max=524288
